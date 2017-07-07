d29c046f4818211bca14a64ec93183d2f11258ce
LainJ <leandrojor1998@gmail.com>
2017-07-06 21:28:25 -0430

    Probando1eraparte
    Oc

---
+++ Hola mundo.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main (void) {
+char saludo [20] = "Hola gente";
+
+printf ("Hola maldito\n\n") ;
+printf("Gracias por utilizar este programa\n\n") ;
+puts(saludo); 
+
+system("pause");
+
+
+return 0;
+}
